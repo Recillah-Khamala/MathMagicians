48b827af87232e010e89e73229dd98fe6d29fdb2
Recillah Khamala <recillahk@gmail.com>
2022-09-17 12:27:29 +0300

    Refactor the Calculator component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,67 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-/* eslint-disable react/destructuring-assignment */
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState(0);
 
-    clickHandle = (e) => {
-      // eslint-disable-next-line react/no-access-state-in-setstate
-      this.setState(calculate(this.state, e.target.value));
-    };
+  const clickHandle = (e) => {
+    setState((state) => calculate(state, e.target.value));
+  };
 
-    render() {
-      return (
-        <div className="calculator">
-          <div className="card">
-            <div className="calcKeys firstRow">
-              <span className="CalcScreen">
-                {this.state.total}
-                {this.state.operation}
-                {this.state.next}
-              </span>
-            </div>
-            <div className="calcKeys secondRow">
-              <input type="button" value="AC" onClick={this.clickHandle} />
-              <input type="button" value="+/-" onClick={this.clickHandle} />
-              <input type="button" value="%" onClick={this.clickHandle} />
-              <input type="button" value="÷" onClick={this.clickHandle} className="operator" />
-            </div>
-            <div className="calcKeys thirdRow">
-              <input type="button" value="7" onClick={this.clickHandle} />
-              <input type="button" value="8" onClick={this.clickHandle} />
-              <input type="button" value="9" onClick={this.clickHandle} />
-              <input type="button" value="x" onClick={this.clickHandle} className="operator" />
-            </div>
-            <div className="calcKeys fourthRow">
-              <input type="button" value="4" onClick={this.clickHandle} />
-              <input type="button" value="5" onClick={this.clickHandle} />
-              <input type="button" value="6" onClick={this.clickHandle} />
-              <input type="button" value="-" onClick={this.clickHandle} className="operator" />
-            </div>
-            <div className="calcKeys fifthRow">
-              <input type="button" value="1" onClick={this.clickHandle} />
-              <input type="button" value="2" onClick={this.clickHandle} />
-              <input type="button" value="3" onClick={this.clickHandle} />
-              <input type="button" value="+" onClick={this.clickHandle} className="operator" />
-            </div>
-            <div className="calcKeys sixthRow">
-              <input type="button" value="0" onClick={this.clickHandle} className="first" />
-              <input type="button" value="." onClick={this.clickHandle} />
-              <input type="button" value="=" onClick={this.clickHandle} className="operator" />
-            </div>
-          </div>
+  return (
+    <div className="calculator">
+      <div className="card">
+        <div className="calcKeys firstRow">
+          <span className="CalcScreen">
+            {state.next || state.operator || state.total || 0}
+          </span>
         </div>
-      );
-    }
-}
-
+        <div className="calcKeys secondRow">
+          <input type="button" value="AC" onClick={clickHandle} />
+          <input type="button" value="+/-" onClick={clickHandle} />
+          <input type="button" value="%" onClick={clickHandle} />
+          <input type="button" value="÷" onClick={clickHandle} className="operator" />
+        </div>
+        <div className="calcKeys thirdRow">
+          <input type="button" value="7" onClick={clickHandle} />
+          <input type="button" value="8" onClick={clickHandle} />
+          <input type="button" value="9" onClick={clickHandle} />
+          <input type="button" value="x" onClick={clickHandle} className="operator" />
+        </div>
+        <div className="calcKeys fourthRow">
+          <input type="button" value="4" onClick={clickHandle} />
+          <input type="button" value="5" onClick={clickHandle} />
+          <input type="button" value="6" onClick={clickHandle} />
+          <input type="button" value="-" onClick={clickHandle} className="operator" />
+        </div>
+        <div className="calcKeys fifthRow">
+          <input type="button" value="1" onClick={clickHandle} />
+          <input type="button" value="2" onClick={clickHandle} />
+          <input type="button" value="3" onClick={clickHandle} />
+          <input type="button" value="+" onClick={clickHandle} className="operator" />
+        </div>
+        <div className="calcKeys sixthRow">
+          <input type="button" value="0" onClick={clickHandle} className="first" />
+          <input type="button" value="." onClick={clickHandle} />
+          <input type="button" value="=" onClick={clickHandle} className="operator" />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Calculator;
