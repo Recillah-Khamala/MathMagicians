1edc4d9ca6154127a92780b85887528899867b2f
Rikstone <tarikbouari@gmail.com>
2022-09-23 03:01:18 -0700

    create test for quote component

--- src/components/components.test.js
+++ src/components/components.test.js
@@ -2,11 +2,13 @@ import { render } from "@testing-library/react";
 import Quote from "../pages/Quotes";
 import Home from "../pages/Home";
 
-
 describe("testing component", () => {
   test("testing Homepage", () => {
     const home = render(<Home />);
     expect(home).toMatchSnapshot();
   });
- 
+  test("testing quote", () => {
+    const quote = render(<Quote />);
+    expect(quote).toMatchSnapshot();
+  });
 });
