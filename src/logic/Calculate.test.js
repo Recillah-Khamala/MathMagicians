1242ecd027bee970782695280c73bd9e6af37cfd
recillah khamala <recillahk@gmail.com>
2022-09-23 12:18:46 +0300

    Add unit tests for Calculate and operatins

---
+++ src/logic/Calculate.test.js
@@ -0,0 +1,13 @@
+import calculate from "./calculate";
+
+describe('calculate', () => {
+    test('should return an object', () => {
+        const data = {
+            total:'21',
+            next: '7',
+            operation: null
+        };
+        const result = { total: '7', next: null, operation: '/'};
+        expect(calculate(data, '/')).toEqual(result);
+    });
+});
